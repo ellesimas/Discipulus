43d778d9349bfc76d2480f8db360caa241324bca
ellesimas <ellesimas@gmail.com>
2020-07-28 22:45:31 +0000

    Adiciona rota que leva para a documentação

--- src/bottle_app.py
+++ src/bottle_app.py
@@ -23,7 +23,11 @@ Neste modulo encontrará:
 '''
 
 from bottle import default_app, route
+from bottle import static_file
+''' Static_file importado para 'chamar' a page html do sphinx em uma das rotas
+'''
 from main import Main
+
 
 ''' na linha 26 importei uma classe presente no caminho
     ellesimas/dev/Discipulus/src/main.py
@@ -41,6 +45,25 @@ def rota_dois():
 def rota_credits():
     """Roteia o caminho /vs para retornar a versão do sistema."""
     return 'Tutorial Discipulus - Versão do sistema: {}'.format(Main().pega_versao())
+
+
+"""@route('/images/<filename:re:.*\.png>') expressão regular anterior.
+   doc é o root
+   .* diz para importar tudo que termine com html.
+   leia roteie qualquer coisa que tenha doc na frente
+"""
+@route('/doc/<filename:re:.*\.html>')
+def rota_doc(filename):
+    """Roteia o caminho /vs para a page html gerada pelo sphinx.
+       Na web <domínio.com/doc/filename/html>
+       Atenção que o doc da chamada deve ser o root não a pasta.
+    """
+    return static_file(filename, root='/home/ellesimas/dev/Discipulus/docs/build/html', mimetype='text/html')
+'''O bottle consegue servir arquivos estáticos.
+   static_file(filename, root='/path/to/image/files', mimetype='image/png')
+   expressão anterior
+
+'''
 
 application = default_app()
 
