5b7866a5f200ee108eddf432f7579fb284955ec2
Emanuelle Simas <ellesimas@gmail.com>
2020-07-27 23:24:04 -0300

    Adiciona documentação no módulo Bottle

--- src/bottle_app.py
+++ src/bottle_app.py
@@ -1,5 +1,26 @@
 
-# A very simple Bottle Hello World app for you to get started with...
+'''
+==============================
+Tutorial Inicial Discipulu's
+==============================
+
+..codeauthor:: Emanuelle Simas
+
+Me encontre em `ellesimas`_.
+
+.. _ellesimas: https://github.com/ellesimas/
+
+
+..version:: 1.0
+
+
+Neste modulo encontrará:
+------------------------
+   As rotas que direcionam para outras abas da web.
+   Utilizei o Framework Bottle que tem sua documentação
+   wncontrável em <https://wiki.python.org/moin/Routing>
+    
+'''
 from bottle import default_app, route
 
 @route('/')
